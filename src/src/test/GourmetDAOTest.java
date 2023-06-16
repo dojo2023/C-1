98ea4b42e8ad55524f537b3f01171b90376063b2
dojo2023-watanabemaika <130334807+dojo2023-watanabemaika@users.noreply.github.com>
2023-06-16 16:59:51 +0900

    Gourmet更新

--- src/src/test/GourmetDAOTest.java
+++ src/src/test/GourmetDAOTest.java
@@ -11,62 +11,106 @@ public class GourmetDAOTest {
 
 		// select()のテスト
 		System.out.println("---------- select()のテスト ----------");
-		List<Gourmet> GourmetList2 = dao.select(new Gourmet());
+
+		String keyWord = "";
+		int favorite = 0;
+		String[] checkedGenre = {"中華", "洋食", "和食", "その他"};
+		List<Gourmet> GourmetList2 = dao.select(keyWord, favorite, checkedGenre);
+		//System.out.println("テスト");
 		for (Gourmet list : GourmetList2) {
-			System.out.println("number：" + list.getnumber());
-			System.out.println("users_number：" + list.getusers_number());
-			System.out.println("name：" + list.getname());
-			System.out.println("branch：" + list.getbranch());
-			System.out.println("genre：" + list.getgenre());
-			System.out.println("reputation：" + list.getreputation());
-			System.out.println("favorite：" + list.getfavorite());
-			System.out.println("memo：" + list.getmemo());
+		//System.out.println("テスト2");
+			System.out.println("number：" + list.getNumber());
+			System.out.println("users_number：" + list.getUsers_number());
+			System.out.println("name：" + list.getName());
+			System.out.println("branch：" + list.getBranch());
+			System.out.println("genre：" + list.getGenre());
+			System.out.println("reputation：" + list.getReputation());
+			System.out.println("favorite：" + list.getFavorite());
+			System.out.println("memo：" + list.getMemo());
 			System.out.println();
 		}
 
-		// insert()のテスト
-		System.out.println("---------- insert()のテスト ----------");
-		Gourmet insRec = new Gourmet("0011", "7", "L営業所", "洋食", "3", "0", "ピザ");
+	// insert_store()のテスト
+		System.out.println("---------- insert_store()のテスト ----------");
 
-		if (dao.insert(insRec)) {
+
+		Gourmet insRecStore = new Gourmet( "Q店", "L営業所", "洋食");
+
+		if (dao.insert_store(insRecStore)) {
 			System.out.println("登録成功！");
-			List<Gourmet> GourmetList3 = dao.select(insRec);
+			List<Gourmet> GourmetList3 = dao.select(keyWord, favorite, checkedGenre);
 			for (Gourmet list : GourmetList3) {
-				System.out.println("number：" + list.getnumber());
-				System.out.println("users_number：" + list.getusers_number());
-				System.out.println("name：" + list.getname());
-				System.out.println("branch：" + list.getbranch());
-				System.out.println("genre：" + list.getgenre());
-				System.out.println("reputation：" + list.getreputation());
-				System.out.println("favorite：" + list.getfavorite());
-				System.out.println("memo：" + list.getmemo());
+
+				System.out.println("name：" + list.getName());
+				System.out.println("branch：" + list.getBranch());
+				System.out.println("genre：" + list.getGenre());
+
+			}
+
+		}
+		else {
+			System.out.println("登録失敗！");
+		}
+
+		// insert_reputation()のテスト
+		System.out.println("---------- insert_reputation()のテスト ----------");
+
+		Gourmet insRecReputetion = new Gourmet(99, 7, 3, 0, "ピザ");
+
+		if (dao.insert_reputation(insRecReputetion)) {
+			System.out.println("登録成功！");
+			List<Gourmet> GourmetList4 = dao.select(keyWord, favorite, checkedGenre);
+			for (Gourmet list : GourmetList4) {
+				System.out.println("number：" + list.getNumber());
+				System.out.println("users_number：" + list.getUsers_number());
+				System.out.println("reputation：" + list.getReputation());
+				System.out.println("favorite：" + list.getFavorite());
+				System.out.println("memo：" + list.getMemo());
 			}
 		}
 		else {
 			System.out.println("登録失敗！");
 		}
 
-		// update()のテスト
-		System.out.println("---------- update()のテスト ----------");
-		Gourmet upRec = new Gourmet("0011", "7", "L営業所", "中華", "3", "0", "麻婆豆腐");
-		if (dao.update(upRec)) {
+		// update_store()のテスト
+		System.out.println("---------- update_store()のテスト ----------");
+
+		Gourmet upRecStore = new Gourmet("Q店", "L営業所", "中華");
+
+		if (dao.update_store(upRecStore,1)) {
 			System.out.println("更新成功！");
-			List<Gourmet> GourmetList4 = dao.select(upRec);
-			for (Gourmet list : GourmetList4) {
-				System.out.println("number：" + list.getnumber());
-				System.out.println("users_number：" + list.getusers_number());
-				System.out.println("name：" + list.getname());
-				System.out.println("branch：" + list.getbranch());
-				System.out.println("genre：" + list.getgenre());
-				System.out.println("reputation：" + list.getreputation());
-				System.out.println("favorite：" + list.getfavorite());
-				System.out.println("memo：" + list.getmemo());
+			List<Gourmet> GourmetList5 = dao.select(keyWord, favorite, checkedGenre);
+			for (Gourmet list : GourmetList5) {
+				System.out.println("name：" + list.getName());
+				System.out.println("branch：" + list.getBranch());
+				System.out.println("genre：" + list.getGenre());
 				System.out.println();
 			}
 		}
 		else {
 			System.out.println("更新失敗！");
 		}
-	}
 
+		// update_reputation()のテスト
+		System.out.println("---------- update_reputation()のテスト ----------");
+
+		Gourmet upRecReputation = new Gourmet(1, 1, 3, 0, "麻婆豆腐");
+
+		if (dao.update_reputation(upRecReputation)) {
+			System.out.println("更新成功！");
+			List<Gourmet> GourmetList6 = dao.select(keyWord, favorite, checkedGenre);
+			for (Gourmet list : GourmetList6) {
+				System.out.println("number：" + list.getNumber());
+				System.out.println("users_number：" + list.getUsers_number());
+				System.out.println("reputation：" + list.getReputation());
+				System.out.println("favorite：" + list.getFavorite());
+				System.out.println("memo：" + list.getMemo());
+				System.out.println();
+			}
+		}
+		else {
+			System.out.println("更新失敗！");
+		}
+
+	}
 }
