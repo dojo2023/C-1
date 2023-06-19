bba175d5575fe1e06596e5a3625922ca904264ca
dojo2023-wakuda <130334620+dojo2023-wakuda@users.noreply.github.com>
2023-06-19 21:57:29 +0900

    LoginServlet

--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UsersDAO;
+import model.LoginUser;
+import model.Users;
 
 /**
  * Servlet implementation class LoginServlet
@@ -16,29 +21,48 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// ログインページに移動する
+		// ログインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 		dispatcher.forward(request, response);
 	}
 
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// メインサーブレットにリダイレクトする
-		response.sendRedirect("/KSHMY/MainServlet");
-	}
+		//リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
 
+		// ログイン処理を行う
+		UsersDAO uDao = new UsersDAO();
+		if (uDao.isLoginOK(new Users(null, null, 0, id, pw, null, null, null))) {	// ログイン成功
+			// セッションスコープにNumberを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("number", new LoginUser(number));    //setAtributeはスコープでデータを設定する際のメソッド
+
+			// メニューサーブレットにリダイレクトする
+			response.sendRedirect("/KSHMY/MainServlet");
+		}
+		else {									// ログイン失敗
+			response.sendRedirect("/KSHMY/LoginServlet");
+			}
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//		// メインサーブレットにリダイレクトする
+		//		response.sendRedirect("/KSHMY/MainServlet");
+		//	}
+
+	}
 }
