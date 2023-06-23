7a5ac2d20ad6ae3ee7013cf70c678a8b78c64fa6
dojo2023-watanabemaika <130334807+dojo2023-watanabemaika@users.noreply.github.com>
2023-06-23 17:55:01 +0900

    修造格納

--- src/src/servlet/MainServlet.java
+++ src/src/servlet/MainServlet.java
@@ -48,29 +48,39 @@ public class MainServlet extends HttpServlet {
 		PrefectureDAO pDAO = new PrefectureDAO();
 		Prefecture prefecture = pDAO.select(prefecture_num);	//ユーザの所属地の県番号に関する緯度経度県名取得
 		request.setAttribute("prefecture",prefecture);
-		
+
 		//ログイン中ユーザの本日の予定表示
 		Date d = new Date();
         SimpleDateFormat d1 = new SimpleDateFormat("yyyy-MM-dd");
-        String start = d1.format(d) + " 00:00:00"; 
-        String end = d1.format(d) + " 23:59:59"; 
-        
+        String start = d1.format(d) + " 00:00:00";
+        String end = d1.format(d) + " 23:59:59";
+
 		CalendarDAO cDAO = new CalendarDAO();
 		List<Calendar> todayList = cDAO.dayselect(user, start,end);
 		request.setAttribute("todayList",todayList);
-		
+
 		String test = "2023-06-22 12:01:00.0";
 		String test2 = test.substring(11, 16);
-		
+
 		System.out.println(test);
 		System.out.println(test2);
-		
+
 		//修造用テキスト
-		String allMsg[] = {"あああああああああああ","いいいいいいいい","うううううううう","えええええええええ"};
+		String allMsg[] = {"崖っぷちありがとう！最高だ！",
+				"君が次に叩く1回で、壁は打ち破れるかもしれないんだ！",
+				"家族は史上最強の味方だ！",
+				"褒め言葉よりも苦言に感謝。",
+				"ナイスボレー、修造！",
+				"人の弱点を見つける天才よりも、人を褒める天才がいい。",
+				"真剣に考えても、深刻になるな！",
+				"反省はしろ！後悔はするな！",
+				"ネクストタイム！",
+				"三日坊主OK！",
+				"これは終わりではなく、新しい修造の始まり。"};
 		Random rnd = new Random();
 		String msg = allMsg[rnd.nextInt(allMsg.length)];
 		request.setAttribute("msg",msg);
-		
+
 		// メインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 		dispatcher.forward(request, response);
